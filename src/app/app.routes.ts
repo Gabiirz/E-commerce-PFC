773c82cbb332059f290ad088b4efee3a765ff47e
gabriel.rezende303 <gabriel.rezende303@gmail.com>
2025-04-14 22:47:10 +0000

    home + carrito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/main/auth/login/login.component';
 import { RegisterComponent } from './components/main/auth/register/register.component';
 import { ProductosComponent } from './components/main/productos/productos.component';
+import { CarritoComponent } from './components/main/carrito/carrito.component';
 
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '**', redirectTo: 'productos' },
+  
   { path: 'productos', component: ProductosComponent },
+  { path: 'carrito', component: CarritoComponent },
 
 
   
--- src/app/components/main/auth/login/login.component.ts
+++ src/app/components/main/auth/login/login.component.ts
@@ -28,7 +28,7 @@ export class LoginComponent {
     console.log(resp)
     if(resp.data.user.role === "authenticated"){
       
-      this.router.navigate(['/'])
+      this.router.navigate(['productos'])
       
     }
    })
